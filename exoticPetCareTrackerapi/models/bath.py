1d0b9634db9b9cbb92642fc489c5f3a77343c8ee
wooldridge12 <wooldridgemikey@gmail.com>
2022-05-25 11:02:39 -0400

    Created model for bath.

--- exoticPetCareTrackerapi/models/bath.py
+++ exoticPetCareTrackerapi/models/bath.py
@@ -1,1 +1,6 @@
 from django.db import models
+
+care_taker = models.ForeignKey("CareTaker", on_delete=models.CASCADE)
+duration = models.IntegerField()
+time = models.TimeField()
+notes = models.CharField(max_length=1000)
