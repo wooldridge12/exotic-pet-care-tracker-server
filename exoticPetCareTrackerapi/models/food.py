c7a2245072c78173540739ea7dc3630eabafa97d
wooldridge12 <wooldridgemikey@gmail.com>
2022-06-07 13:19:26 -0400

    Created the model for food.

--- exoticPetCareTrackerapi/models/food.py
+++ exoticPetCareTrackerapi/models/food.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+class Food(models.Model):
+    # care_taker = models.ForeignKey("CareTaker", on_delete=models.CASCADE)
+    time = models.TimeField()
+    Quantity = models.IntegerField()
+    food = models.ForeignKey("FoodType", on_delete=models.CASCADE)
+    
