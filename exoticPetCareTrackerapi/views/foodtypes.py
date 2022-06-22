37395cb0f034fa1bb615711110ebccb359021dd1
wooldridge12 <wooldridgemikey@gmail.com>
2022-06-22 11:39:30 -0400

    Created the foodtypes.py file and have all imports.

---
+++ exoticPetCareTrackerapi/views/foodtypes.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponseServerError
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from exoticPetCareTrackerapi.models import FoodType
+
